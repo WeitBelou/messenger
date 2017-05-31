67d7107f50c5800389d9eceb03d63965cec4d611
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-06-01 02:34:35 +0300

    Add simple rest api for messages.

---
+++ src/main/java/edu/phystech/kosolapov/ivan/messenger/MessengerServer.java
@@ -0,0 +1,11 @@
+package edu.phystech.kosolapov.ivan.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerServer {
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerServer.class, args);
+    }
+}
---
+++ src/main/java/edu/phystech/kosolapov/ivan/messenger/message/MessageRepository.java
@@ -0,0 +1,7 @@
+package edu.phystech.kosolapov.ivan.messenger.message;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+
+}
