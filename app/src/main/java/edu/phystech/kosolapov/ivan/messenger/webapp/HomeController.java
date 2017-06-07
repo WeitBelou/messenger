1dccbcc2230e31530b5908e69d6f18e8a256f82c
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-06-07 20:14:51 +0300

    Fix order of annotations.

--- app/src/main/java/edu/phystech/kosolapov/ivan/messenger/webapp/HomeController.java
+++ app/src/main/java/edu/phystech/kosolapov/ivan/messenger/webapp/HomeController.java
@@ -3,10 +3,9 @@ package edu.phystech.kosolapov.ivan.messenger.webapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/home")
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
     @GetMapping
