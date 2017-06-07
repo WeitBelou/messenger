ba1d67b29c1eb8d2767f1104c85d33edc3a2298f
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-06-07 19:14:29 +0300

    Change redirect approach.

--- app/src/main/java/edu/phystech/kosolapov/ivan/messenger/webapp/RedirectController.java
+++ app/src/main/java/edu/phystech/kosolapov/ivan/messenger/webapp/RedirectController.java
@@ -3,14 +3,13 @@ package edu.phystech.kosolapov.ivan.messenger.webapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/")
 public class RedirectController {
 
     @GetMapping
-    public RedirectView redirectToHome() {
-        return new RedirectView("home");
+    public String redirectToHome() {
+        return "redirect:/home";
     }
 }
