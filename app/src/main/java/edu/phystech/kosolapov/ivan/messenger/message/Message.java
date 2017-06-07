524a83ca405ed0c497d1658c3fe515a5cad401ef
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-06-07 21:05:14 +0300

    Update message representation.

--- app/src/main/java/edu/phystech/kosolapov/ivan/messenger/message/Message.java
+++ app/src/main/java/edu/phystech/kosolapov/ivan/messenger/message/Message.java
@@ -1,11 +1,14 @@
 package edu.phystech.kosolapov.ivan.messenger.message;
 
 import lombok.Data;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.sql.Date;
 
 @Entity
 @Data
@@ -16,4 +19,10 @@ class Message {
 
     private String author;
     private String content;
+
+    @Generated(value = GenerationTime.INSERT)
+    private Date createdAt;
+
+    @Generated(value = GenerationTime.ALWAYS)
+    private Date updatedAt;
 }
