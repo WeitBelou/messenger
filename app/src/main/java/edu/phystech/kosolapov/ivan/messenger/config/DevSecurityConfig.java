c5e174e96527da4ffd2a4052bc2263377ebc4d0d
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-06-25 11:04:33 +0300

    Switch to temporary development security config.

--- app/src/main/java/edu/phystech/kosolapov/ivan/messenger/config/DevSecurityConfig.java
+++ app/src/main/java/edu/phystech/kosolapov/ivan/messenger/config/DevSecurityConfig.java
@@ -1,15 +1,15 @@
 package edu.phystech.kosolapov.ivan.messenger.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Profile("dev")
+public class DevSecurityConfig extends WebSecurityConfigurerAdapter {
 
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/api/**").hasRole("USER")
-                .and().httpBasic();
+        http.authorizeRequests().antMatchers("/**").permitAll();
     }
 }
